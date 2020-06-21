edc70873a2247daf04e6986cb8a7eb4c7c23a6a8
Palak2033 <66773262+Palak2033@users.noreply.github.com>
2020-06-21 15:05:31 +0530

    improved Ui added

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-var express = require("express"),
-	bodyParser = require("body-parser"),
+var express = require('express'),
+	bodyParser = require('body-parser'),
 	app = express(),
-	seedDb = require("./seed"),
-	bodyParser = require("body-parser"),
-	mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/vms", {
+	seedDb = require('./seed'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/vms', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
+	useCreateIndex: true
 });
-require("dotenv").config();
+require('dotenv').config();
 
-app.listen("3000", function (req, res) {
-	console.log("VMS server has started");
+app.listen('3000', function(req, res) {
+	console.log('VMS server has started');
 });
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 // seedDb();
-app.use(require("./routes/resident"));
-app.use(require("./routes/security"));
+app.use(require('./routes/resident'));
+app.use(require('./routes/security'));
 
-app.get("*", function (req, res) {
-	res.send("404 Page Not Found");
+app.get('*', function(req, res) {
+	res.send('404 Page Not Found');
 });
