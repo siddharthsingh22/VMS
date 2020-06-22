22f9b71049d6da90f93ebf52fd7d7885b1e5f77a
siddharthsingh22 <siddharthpratapsingh21@gmail.com>
2020-06-22 21:22:45 +0530

    security section of the admin portal completed.

--- models/security.js
+++ models/security.js
@@ -6,8 +6,17 @@ const securitySchema = new mongoose.Schema({
 	password: { type: String, required: true },
 	phoneNo: { type: Number },
 	address: { type: String },
+	aadharNo: { type: String, required: true },
+	designation: { type: String, required: true },
+	remarks: { type: String },
+	image: { type: Buffer, required: true },
+	imageType: { type: String, required: true },
 });
-
+securitySchema.virtual("securityImagePath").get(function () {
+	if (this.image != null && this.imageType != null) {
+		return `data:${this.imageType};charset=utf-8;base64,${this.image.toString("base64")}`;
+	}
+});
 const Securities = mongoose.model("Security", securitySchema);
 
 module.exports = Securities;
